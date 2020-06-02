89082970527aeed0b2ed364fa99932ffe6f5550b
David Hettler <david-hettler@gmx.de>
2020-06-02 22:07:51 +0200

    Build image when parent image is updated

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 image_name=dhet/debezium-connect
-tag=$1
 
-if [[ $# -ne 1 ]] ; then
-  echo "Please specify an image tag."
-  exit
-fi
+for tag in $(echo "$@" | tr "," "\n")
+do
+  echo "===> Building image with tag '$tag'"
+  sed -e "s/{{tag}}/${tag}/g" docker/Dockerfile.template > docker/Dockerfile
+  docker build -t "$image_name:$tag" -t $image_name:latest docker
 
-sed -e "s/{{tag}}/${tag}/g" docker/Dockerfile.template > docker/Dockerfile
-
-docker build -t $image_name:$1 -t $image_name:latest docker
-docker push $image_name:$1
-docker push $image_name:latest
+  echo "===> Pushing image with tag '$tag'"
+  docker push "$image_name:$tag"
+done
